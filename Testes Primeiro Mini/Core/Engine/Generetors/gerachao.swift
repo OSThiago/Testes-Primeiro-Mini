04fedc7f1055ad1c924b5602fe09603defdf498d
Thiago de Oliveira <thiagos2000@gmail.com>
2021-07-14 19:27:51 -0300

    alteração do bloco do chão e bug de falta de chão arrumado

--- Testes Primeiro Mini/Core/Engine/Generetors/gerachao.swift
+++ Testes Primeiro Mini/Core/Engine/Generetors/gerachao.swift
@@ -44,41 +44,40 @@ extension GameScene{
             let _ground = self.createGround(position: initialPosition)
             self.addChild(_ground)
             self.moveGround(node: _ground, time: time)
+            
+            
         }
         
-        
-        let waitInBetween = SKAction.wait(forDuration: time)//0.07
+        let waitInBetween = SKAction.wait(forDuration: time*0.07)//0.07
         let sequence = SKAction.sequence([creatGround,waitInBetween])
-        //let repeatForever = SKAction.repeatForever(sequence)
-        self.run(sequence)
+        let repeatForever = SKAction.repeatForever(sequence)
+        self.run(repeatForever)
     }
     
     func moveGround(node: SKSpriteNode, time: TimeInterval) {
         
         let multiplier = 4.0
         let moveAction = SKAction.moveBy(x: size.width*(-2.8), y: 0, duration: time*multiplier)
-        let chamarCriacao = SKAction.run {
-            self.generateGrounds(time: time)
-        }
         
         let remover = SKAction.removeFromParent()
-        let sequencia = SKAction.sequence([moveAction,remover,chamarCriacao])
+        let sequencia = SKAction.sequence([moveAction,remover])
         node.run(sequencia)
     }
     
     func initialGround(time: TimeInterval) {
-        // Posição inicila no centro da tela
-        let initialPosition = CGPoint(x: size.width*0.48, y: size.height*0.5)
-               
-            //Criando o chão
-            let ground = self.createGround(position: initialPosition)
-                   
-            //Adicionando o chão na tela
-            self.addChild(ground)
-                   
-            //Colocando o chão para se mover
-            self.moveGround(node: ground, time: time)
+        
+        // Criar blocos que preenche a tela ao iniciar
+        for i in 0...29 {
+            let initialPosition = CGPoint(x: size.width*(CGFloat(i)*0.05), y: size.height*0.5)
+                //Criando o chão
+                let ground = self.createGround(position: initialPosition)
+                       
+                //Adicionando o chão na tela
+                self.addChild(ground)
+                       
+                //Colocando o chão para se mover
+                self.moveGround(node: ground, time: time)
+        }
     }
-    
     
 }
