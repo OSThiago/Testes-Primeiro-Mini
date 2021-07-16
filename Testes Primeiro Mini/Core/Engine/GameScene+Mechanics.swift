904aaaafe2836b746a53bf6e9969f63918750729
Thiago de Oliveira <thiagos2000@gmail.com>
2021-07-16 03:00:01 -0300

    Modularizando a função de swipe

--- Testes Primeiro Mini/Core/Engine/GameScene+Mechanics.swift
+++ Testes Primeiro Mini/Core/Engine/GameScene+Mechanics.swift
@@ -28,9 +28,9 @@ extension GameScene{
         case .up:
             print("SwipeUP")
             if ( personagem.player.yScale < 0){
-                personagem.player.position = CGPoint(x: size.width*(0.5), y: size.height*(0.63))
-                personagem.player.yScale = personagem.player.yScale*(-1)
-                physicsWorld.gravity.dy = physicsWorld.gravity.dy*(-1)
+                
+                reversePlayer(heightPosition: 0.63)
+
             }else{
                 personagem.player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: CGFloat(personagem.player.size.height*0.22)))
             }
@@ -38,9 +38,9 @@ extension GameScene{
         case .down:
             print("SwipeDown")
             if ( personagem.player.yScale > 0){
-                personagem.player.position = CGPoint(x: size.width*(0.5), y: size.height*(0.39))
-                personagem.player.yScale = personagem.player.yScale*(-1)
-                physicsWorld.gravity.dy = physicsWorld.gravity.dy*(-1)
+                
+                reversePlayer(heightPosition: 0.39)
+                
             }else{
                 personagem.player.physicsBody?.applyImpulse(CGVector(dx: 0, dy: CGFloat(personagem.player.size.height*(-0.22))))
             }
@@ -48,4 +48,12 @@ extension GameScene{
             print("No Direction")
         }
     }
+    
+    
+    func reversePlayer(heightPosition: CGFloat) {
+        personagem.player.position = CGPoint(x: size.width*(0.5), y: size.height*(heightPosition))
+        personagem.player.yScale = personagem.player.yScale*(-1)
+         physicsWorld.gravity.dy = physicsWorld.gravity.dy*(-1)
+    }
+    
 }
